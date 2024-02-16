80a33d06805aa003813f60677a353abae35a9d92
Mark Fynes <tfynes@vmware.com>
2024-02-16 09:50:38 -0500

    .

--- src/main/java/com/fynesy/n1/n1/N1Application.java
+++ src/main/java/com/fynesy/n1/n1/N1Application.java
@@ -2,12 +2,22 @@ package com.fynesy.n1.n1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class N1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(N1Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		String s = "hi there\n";
+		System.out.println(s);
+		return s;
 	}
 
 }
